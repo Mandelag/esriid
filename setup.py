a4fbaab3d7264dec710983ebc4babf2406f9573a
Keenan Gebze <keenan.gebze@gmail.com>
2018-09-20 10:14:51 +0700

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='esriid',
+      version='0.1',
+      description='Python script and utilities used by Esri Indonesia',
+      url='http://github.com/Mandelag/esriid',
+      author='Keenan Gebze',
+      author_email='keenan.gebze@gmail.com',
+      license='MIT',
+      packages=['esriid'],
+      zip_safe=False)
